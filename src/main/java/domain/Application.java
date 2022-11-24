21bf0a673f02aabd21a0e1cbfebe7bf70d63c28d
swandevson <shai4956@gmail.com>
2022-11-25 01:29:55 +0900

    Feat(Application): 플레이어 이름, 베팅 금액을 통한 플레이어들 생성

---
+++ src/main/java/domain/Application.java
@@ -0,0 +1,37 @@
+package domain;
+
+import domain.View.InputView;
+import domain.user.Dealer;
+import domain.user.Player;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+
+public class Application {
+    private static InputView inputView = new InputView();
+
+    private static Player createPlayer(String playerName) {
+        double bettingMoney = inputView.readBettingMoney();
+
+        return new Player(playerName, bettingMoney);
+    }
+
+    private static List<Player> createPlayers() {
+        List<String> playersName = inputView.readPlayers();
+        List<Player> players = new ArrayList<>(playersName.size());
+
+        for (String playerName : playersName) {
+            Player player = createPlayer(playerName);
+            players.add(player);
+        }
+
+        return Collections.unmodifiableList(players);
+    }
+
+    public static void main(String[] args) {
+        List<Player> players = createPlayers();
+        Dealer dealer = new Dealer();
+    }
+}
