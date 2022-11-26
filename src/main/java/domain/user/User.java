73bc43425fd845dab7b6eb937e0ceaf2ec7185ad
yoondgu <doyou2ing@naver.com>
2022-11-26 13:39:57 +0900

    :sparkles:feat(Domain-4): 사용자가 현재 가지고 있는 카드의 합을 계산하는 기능 구현
    - User 클래스를 정의하여 Player와 Dealer가 이를 상속하도록 수정함.
    - User 클래스에서 두 클래스의 공통 기능을 구현하도록 함.
    - 단위테스트 작성 및 통과 확인함.

--- src/main/java/domain/user/User.java
+++ src/main/java/domain/user/User.java
@@ -12,4 +12,23 @@ public class User {
     public void addCard(Card card) {
         cards.add(card);
     }
+
+    public int addAllScore() {
+        int totalScore = cards.stream()
+                .map(Card::getScore)
+                .reduce(0, Integer::sum);
+        return addMoreOrNotByAce(totalScore, hasAce(cards));
+    }
+
+    private int addMoreOrNotByAce(int totalScore, boolean hasAce) {
+        if (totalScore < 12 && hasAce) {
+            return totalScore + 10;
+        }
+        return totalScore;
+    }
+
+    private boolean hasAce(List<Card> cards) {
+        return cards.stream()
+                .anyMatch(Card::isAce);
+    }
 }
---
+++ src/test/java/domain/user/UserTest.java
@@ -0,0 +1,40 @@
+package domain.user;
+
+import static org.assertj.core.api.Assertions.*;
+
+import domain.card.Card;
+import domain.card.Symbol;
+import domain.card.Type;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
+
+class UserTest {
+
+    @DisplayName("카드 합 계산: Ace를 가지고 있는 경우, 21을 넘지 않거나 21에 더 가까운 합으로 계산")
+    @ParameterizedTest
+    @MethodSource("generateStreamOfCardsAndExpectedScore")
+    void addAllScore(List<Card> cards, int expectedScore) {
+        User user = new User();
+        cards.forEach(user::addCard);
+
+        assertThat(user.addAllScore()).isEqualTo(expectedScore);
+    }
+
+    private static Stream<Arguments> generateStreamOfCardsAndExpectedScore() {
+        List<Arguments> arguments = new LinkedList<>();
+        Card aceCard1 = new Card(Symbol.ACE, Type.CLUB);
+        Card aceCard2 = new Card(Symbol.ACE, Type.DIAMOND);
+        arguments.add(Arguments.of(List.of(aceCard1, aceCard2),12));
+        arguments.add(Arguments.of(List.of(aceCard1, new Card(Symbol.FIVE, Type.CLUB)), 16));
+        arguments.add(Arguments.of(List.of(aceCard1, new Card(Symbol.TEN, Type.CLUB)),21));
+        arguments.add(Arguments.of(
+                List.of(aceCard1, new Card(Symbol.TEN, Type.CLUB), new Card(Symbol.TEN, Type.DIAMOND)), 21));
+        return arguments.stream();
+    }
+}
