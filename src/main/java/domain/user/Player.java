38fb25b844bd4de7d4cdc42b0b37f12a51acd8f4
yoondgu <doyou2ing@naver.com>
2022-11-26 13:35:53 +0900

    :recycle:refactor: Player와 Dealer의 공통 필드, 메소드를 부모 클래스 User로 분리

--- src/main/java/domain/user/Player.java
+++ src/main/java/domain/user/Player.java
@@ -1,18 +1,13 @@
 package domain.user;
 
-import domain.card.Card;
 import model.validator.GameRuleValidator;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * 게임 참여자를 의미하는 객체
  */
-public class Player {
+public class Player extends User {
     private final String name;
     private final double bettingMoney;
-    private final List<Card> cards = new ArrayList<>();
 
     public Player(String name, double bettingMoney) {
         GameRuleValidator.validateInitialBettingMoney(bettingMoney);
@@ -20,10 +15,5 @@ public class Player {
         this.bettingMoney = bettingMoney;
     }
 
-    public void addCard(Card card) {
-        cards.add(card);
-    }
-
     // TODO 추가 기능 구현
-
 }
