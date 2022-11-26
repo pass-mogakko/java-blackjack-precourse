95c50539cdb289c9df35e58cf447794bf0ea157d
yoondgu <doyou2ing@naver.com>
2022-11-27 01:14:57 +0900

    :sparkles:feat(GameController): Domain-1, UI-1, UI-2를 실제 수행하는 코드 작성

---
+++ src/main/java/controller/GameController.java
@@ -0,0 +1,40 @@
+package controller;
+
+import static domain.rule.BettingRule.INITIAL_MAX_VALUE;
+import static domain.rule.BettingRule.INITIAL_MIN_VALUE;
+import static domain.rule.ParticipantsRule.PARTICIPANTS_MAX_COUNT;
+import static domain.rule.ParticipantsRule.PARTICIPANTS_MIN_COUNT;
+import static view.resource.OutputContent.FORMAT_ASK_BETTING_MONEY;
+import static view.resource.OutputContent.MESSAGE_ASK_PLAYERS_NAME;
+
+import model.BlackJackGame;
+import view.InputView;
+import view.OutputView;
+
+import java.util.List;
+
+public class GameController {
+
+    private final InputView inputView = new InputView();
+    private final OutputView outputView = new OutputView();
+    private final BlackJackGame blackJackGame = new BlackJackGame();
+
+    public void run() {
+        List<String> playerNames = askPlayersName();
+        playerNames.forEach(this::enrollWithBettingMoney);
+    }
+
+    private List<String> askPlayersName() {
+        outputView.printMessage(MESSAGE_ASK_PLAYERS_NAME);
+        List<String> playerNames = inputView.readPlayersName(PARTICIPANTS_MIN_COUNT.getValue(), PARTICIPANTS_MAX_COUNT.getValue());
+        outputView.printBlankLine();
+        return playerNames;
+    }
+
+    private void enrollWithBettingMoney(String playerName) {
+        outputView.printFormattedMessage(FORMAT_ASK_BETTING_MONEY, playerName);
+        double bettingMoney = inputView.readBettingMoney(INITIAL_MIN_VALUE.getValue(), INITIAL_MAX_VALUE.getValue());
+        blackJackGame.enrollPlayer(playerName, bettingMoney);
+        outputView.printBlankLine();
+    }
+}
