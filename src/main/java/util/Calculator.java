fa202dd2772ba6f5d3ce44e33b86216ff142ef0f
송민진 <m_6595@naver.com>
2022-11-27 00:14:44 +0900

    feat(blackjack): optimize score with ace card
    구현 - ACE 카드가 있다면, 21에 가깝도록 1 혹은 11 중에 자동 선택하는 기능 구현

--- src/main/java/util/Calculator.java
+++ src/main/java/util/Calculator.java
@@ -5,11 +5,31 @@ import domain.card.Card;
 import java.util.List;
 
 public class Calculator {
+
     public int addAllCardScore(List<Card> cards) {
         int sum = 0;
         for (Card card : cards) {
-            sum += card.getSymbolScore();
+            int score = card.getSymbolScore();
+            sum += score;
+        }
+        if (hasAce(cards))
+            sum = optimizeAce(sum);
+        return sum;
+    }
+
+    private int optimizeAce(int sum) {
+        if (sum <= 11) {
+            sum += 10;
         }
         return sum;
+    }
+
+    private boolean hasAce(List<Card> cards) {
+        for (Card card : cards) {
+            if (card.getSymbolScore() == 1) {
+                return true;
+            }
+        }
+        return false;
     }
 }
