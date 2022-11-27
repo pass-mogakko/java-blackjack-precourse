be228fae76c1d80989d97fab2113ce771b165db8
Isu Shin <isu010614@naver.com>
2022-11-27 19:48:18 +0900

    refactor: 덱의 카드 삭제, 탐색 기능 클래스 이동

--- src/main/java/domain/BlackJackGame.java
+++ src/main/java/domain/BlackJackGame.java
@@ -10,20 +10,28 @@ import java.util.Random;
 
 public class BlackJackGame {
     private static final int CARD_NUMBER = 51;
-    private static final HashMap<Integer, Card> deck = new HashMap<>();
-    private static final CardFactory cardFactory = new CardFactory(deck);
+    private static final CardFactory cardFactory = new CardFactory();
+    private static final HashMap<Integer, Card> deck = cardFactory.createDeck();
 
     public static void run() {
 
 
     }
 
-    private static int getRandomNumber() {
+    private int getRandomNumber() {
         Random random = new Random();
         int index = random.nextInt(CARD_NUMBER);
-        while(!cardFactory.findNumber(index)){
+        while(!findNumber(index)){
             index = random.nextInt(CARD_NUMBER);
         }
         return index;
+    }
+
+    private boolean findNumber(int index) {
+        return deck.containsKey(index);
+    }
+
+    private void deleteCard(int index) {
+        deck.remove(index);
     }
 }
