1af81c9520b022c3cadfcabdaeb8f4a913730719
Isu Shin <isu010614@naver.com>
2022-11-27 18:42:19 +0900

    feat: 베팅 금액을 입력받는 기능

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -11,7 +11,11 @@ import java.util.StringTokenizer;
 
 public class InputView {
     private static final String INPUT_PLAYER_NAME = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
-    private static final String NAME_REGEX = "[a-z]";
+    private static final String INPUT_BETTING_MONEY = "의 베팅금액은?";
+    private static final String NAME_REGEX = "^[a-z]+$";
+    private static final String MONEY_REGEX = "^[0-9]+$";
+    private static final int START_MONEY_RANGE = 0;
+    private static final int END_MONEY_RANGE = 100_000_000;
 
     private static final BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 
@@ -31,6 +35,13 @@ public class InputView {
         if (!name.matches(NAME_REGEX)) {
             throw new IllegalArgumentException(ExceptionMessage.ONLY_ALPHABET);
         }
+    }
+
+    public static int readBettingMoney(String name) throws IOException {
+        System.out.println(name + INPUT_BETTING_MONEY);
+        String bettingMoney = console.readLine();
+
+        return Integer.parseInt(bettingMoney);
     }
 
 }
