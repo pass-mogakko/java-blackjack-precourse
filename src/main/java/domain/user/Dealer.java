6c01cf38e4abbced747b9cab66273cf561bfd100
Isu Shin <isu010614@naver.com>
2022-11-27 22:42:18 +0900

    feat: 딜러 카드 리턴하는 기능

--- src/main/java/domain/user/Dealer.java
+++ src/main/java/domain/user/Dealer.java
@@ -4,17 +4,31 @@ import domain.card.Card;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 게임 딜러를 의미하는 객체
  */
 public class Dealer {
+    private final double money;
     private final List<Card> cards = new ArrayList<>();
 
-    public Dealer() {}
+    public Dealer(double money) {
+        this.money = money;
+    }
 
     public void addCard(Card card) {
         cards.add(card);
+    }
+
+    public String getInitialDealerCard(){
+        return cards.get(0).toString();
+    }
+
+    public String getDealerCards(){
+        return cards.stream()
+                .map(Card::toString)
+                .collect(Collectors.joining(", "));
     }
 
     // TODO 추가 기능 구현
