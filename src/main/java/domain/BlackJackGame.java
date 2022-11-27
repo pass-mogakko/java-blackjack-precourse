2085bc6d11a8470495e160c1184b1c142bc0c424
Isu Shin <isu010614@naver.com>
2022-11-27 19:36:52 +0900

    refactor: 랜덤번호 생성 수정
    - 랜덤 번호 생성 시 덱에 포함되어 있는지 확인 후 리턴

--- src/main/java/domain/BlackJackGame.java
+++ src/main/java/domain/BlackJackGame.java
@@ -2,21 +2,28 @@ package domain;
 
 import domain.card.Card;
 import domain.card.CardFactory;
+import domain.user.Dealer;
+import domain.user.Player;
 
 import java.util.HashMap;
 import java.util.Random;
 
 public class BlackJackGame {
     private static final int CARD_NUMBER = 51;
+    private static final HashMap<Integer, Card> deck = new HashMap<>();
+    private static final CardFactory cardFactory = new CardFactory(deck);
 
-    public static void run(){
-        HashMap<Integer, Card> deck = new HashMap<>();
-        CardFactory cardFactory = new CardFactory(deck);
+    public static void run() {
+
 
     }
 
-    private static int getRandomNumber(){
+    private static int getRandomNumber() {
         Random random = new Random();
-        return random.nextInt(CARD_NUMBER);
+        int index = random.nextInt(CARD_NUMBER);
+        while(!cardFactory.findNumber(index)){
+            index = random.nextInt(CARD_NUMBER);
+        }
+        return index;
     }
 }
