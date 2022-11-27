e0a621f2baa1c526e0cd074c574754e42f1f04ef
swandevson <shai4956@gmail.com>
2022-11-27 22:50:44 +0900

    Feat(OutputView): 최종 수익 결과 출력

--- src/main/java/domain/View/OutputView.java
+++ src/main/java/domain/View/OutputView.java
@@ -11,10 +11,15 @@ import java.util.stream.Collectors;
 
 
 public class OutputView {
-    private static final String DEALER_CARDS = "딜러:%s" + System.lineSeparator();
-    private static final String PLAYER_CARDS = "%s카드:%s" + System.lineSeparator();
-    private static final String DEALER_RESULT = "딜러 카드:%s - 결과: %d" + System.lineSeparator();
-    private static final String PLAYER_RESULT = "%s카드:%s - 결과: %d" + System.lineSeparator();
+    private static final String DELIMITER = ",";
+    private static final String DEALER_CARDS = "딜러: %s" + System.lineSeparator();
+    private static final String PLAYER_CARDS = "%s카드: %s" + System.lineSeparator();
+    private static final String DEALER_RESULT = "딜러 카드: %s - 결과: %d" + System.lineSeparator();
+    private static final String PLAYER_RESULT = "%s카드: %s - 결과: %d" + System.lineSeparator();
+    private static final String DEALER_PROFIT = "딜러: %.0f" + System.lineSeparator();
+    private static final String PLAYER_PROFIT = "%s: %.0f" + System.lineSeparator();
+
+
 
     public void printDealerCards(List<Card> cards) {
         System.out.printf(DEALER_CARDS, cards.stream().
@@ -54,6 +59,15 @@ public class OutputView {
         printDealerResultCards(dealer.getCards());
         for (Player player : players) {
             printPlayerResultCards(player.getCards(), player.getName());
+        }
+    }
+
+    public void printProfits(Dealer dealer, List<Player> players) {
+        System.out.println("##최종 수익");
+
+        System.out.printf(DEALER_PROFIT, dealer.calculateProfit(players));
+        for (Player player : players) {
+            System.out.printf(PLAYER_PROFIT, player.getName(), player.calculateProfit(dealer));
         }
     }
 }
