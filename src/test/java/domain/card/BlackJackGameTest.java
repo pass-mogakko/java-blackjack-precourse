f5762931f175cee84fe198df361007319270eea9
Isu Shin <isu010614@naver.com>
2022-11-27 23:43:42 +0900

    test: 딜러와 플레이어의 카드가 두장씩 나누어지는지 확인하는 코드

--- src/test/java/domain/card/BlackJackGameTest.java
+++ src/test/java/domain/card/BlackJackGameTest.java
@@ -6,7 +6,6 @@ import domain.user.Player;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,10 +18,26 @@ public class BlackJackGameTest {
 
     @DisplayName("카드가 덱에서 제거되는지 확인")
     @Test
-    void create() {
-        BlackJackGame game = new BlackJackGame(players,dealer);
+    void removeCardFromDeck() {
+        BlackJackGame game = new BlackJackGame(players, dealer);
         game.handOutFirstTwoCards();
         assertThat(game.getDeckSize())
                 .isEqualTo(46);
+    }
+
+    @DisplayName("플레이어의 카드가 두장씩 나누어졌는지 확인")
+    @Test
+    void checkHandoutPlayerTwoCards() {
+        BlackJackGame game = new BlackJackGame(players, dealer);
+        game.handOutFirstTwoCards();
+        assertThat(player1.getCardsInList()).hasSize(2);
+    }
+
+    @DisplayName("딜러의 카드가 두장 주어졌는지 확인")
+    @Test
+    void checkHandoutDealerTwoCards() {
+        BlackJackGame game = new BlackJackGame(players, dealer);
+        game.handOutFirstTwoCards();
+        assertThat(player1.getCardsInList()).hasSize(2);
     }
 }
