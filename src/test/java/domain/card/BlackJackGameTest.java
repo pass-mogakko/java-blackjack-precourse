9e2bb4e432565232e9b3533d202405b072c9d507
Isu Shin <isu010614@naver.com>
2022-11-28 01:59:56 +0900

    test: 테스트코드 리팩터링

--- src/test/java/domain/card/BlackJackGameTest.java
+++ src/test/java/domain/card/BlackJackGameTest.java
@@ -6,19 +6,24 @@ import domain.user.Player;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BlackJackGameTest {
-    private final Player player1 = new Player("pobi", 300);
-    private final Player player2 = new Player("suan", 500);
-    private final List<Player> players = List.of(player1, player2);
+    private final List<Player> players = new ArrayList<>();
     private final Dealer dealer = new Dealer(0);
+
+    private void addPlayers() {
+        players.add(new Player("pobi", 300));
+        players.add(new Player("suan", 500));
+    }
 
     @DisplayName("카드가 덱에서 제거되는지 확인")
     @Test
     void removeCardFromDeck() {
+        addPlayers();
         BlackJackGame game = new BlackJackGame(players, dealer);
         game.handOutFirstTwoCards();
         assertThat(game.getDeckSize())
@@ -28,9 +33,12 @@ public class BlackJackGameTest {
     @DisplayName("플레이어의 카드가 두장씩 나누어졌는지 확인")
     @Test
     void checkHandoutPlayerTwoCards() {
+        addPlayers();
         BlackJackGame game = new BlackJackGame(players, dealer);
         game.handOutFirstTwoCards();
-        assertThat(player1.getCardsInList()).hasSize(2);
+        assertThat(players.get(0)
+                .getCardsInList())
+                .hasSize(2);
     }
 
     @DisplayName("딜러의 카드가 두장 주어졌는지 확인")
@@ -38,6 +46,6 @@ public class BlackJackGameTest {
     void checkHandoutDealerTwoCards() {
         BlackJackGame game = new BlackJackGame(players, dealer);
         game.handOutFirstTwoCards();
-        assertThat(player1.getCardsInList()).hasSize(2);
+        assertThat(dealer.getCardsInList()).hasSize(2);
     }
 }
