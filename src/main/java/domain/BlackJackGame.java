7c969331142150872d6ce071605fd7820b8a0ec0
Isu Shin <isu010614@naver.com>
2022-11-27 19:24:26 +0900

    feat: 랜덤 번호 생성 기능

--- src/main/java/domain/BlackJackGame.java
+++ src/main/java/domain/BlackJackGame.java
@@ -4,11 +4,19 @@ import domain.card.Card;
 import domain.card.CardFactory;
 
 import java.util.HashMap;
+import java.util.Random;
 
 public class BlackJackGame {
-    private static final HashMap<Integer, Card> deck = CardFactory.createDeck();
+    private static final int CARD_NUMBER = 51;
 
     public static void run(){
+        HashMap<Integer, Card> deck = new HashMap<>();
+        CardFactory cardFactory = new CardFactory(deck);
 
+    }
+
+    private static int getRandomNumber(){
+        Random random = new Random();
+        return random.nextInt(CARD_NUMBER);
     }
 }
