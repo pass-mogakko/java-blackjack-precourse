9b1784b176cebf9a773456e976dffef9e642e6a4
Isu Shin <isu010614@naver.com>
2022-11-27 20:21:59 +0900

    feat: 딜러, 플레이어에게 초기 카드 나눠주는 기능

--- src/main/java/domain/BlackJackGame.java
+++ src/main/java/domain/BlackJackGame.java
@@ -10,21 +10,37 @@ import java.util.HashMap;
 import static domain.card.RandomNumber.getRandomNumber;
 
 public class BlackJackGame {
+    private final Player player;
+    private final Dealer dealer;
+
     private static final CardFactory cardFactory = new CardFactory();
     private static final HashMap<Integer, Card> deck = cardFactory.createDeck();
+    private static final int FIRST_CARD_COUNT = 2;
 
-    public static void run() {
+    public BlackJackGame(Player player, Dealer dealer) {
+        this.player = player;
+        this.dealer = dealer;
+    }
 
+    public void run() {
+        handOutFirstTwoCards();
 
     }
 
-    private void handOutPlayerCard(Player player) {
+    private void handOutFirstTwoCards() {
+        for(int count = 0; count < FIRST_CARD_COUNT; count++){
+            handOutPlayerCard();
+            handOutDealerCard();
+        }
+    }
+
+    private void handOutPlayerCard() {
         final int index = getRandomNumber(deck);
         player.addCard(deck.get(index));
         deleteCard(index);
     }
 
-    private void handOutDealerCard(Dealer dealer) {
+    private void handOutDealerCard() {
         final int index = getRandomNumber(deck);
         dealer.addCard(deck.get(index));
         deleteCard(index);
