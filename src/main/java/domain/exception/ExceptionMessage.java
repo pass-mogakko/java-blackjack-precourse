e78ca8f84a924794fbd20f43caa16c4d7bad4f63
Isu Shin <isu010614@naver.com>
2022-11-28 02:28:51 +0900

    feat: 에러 메시지 추가

--- src/main/java/domain/exception/ExceptionMessage.java
+++ src/main/java/domain/exception/ExceptionMessage.java
@@ -4,4 +4,5 @@ public class ExceptionMessage {
     public static final String ONLY_ALPHABET = "[ERROR] 알파벳 소문자만 입력할 수 있습니다.";
     public static final String ONLY_NUMBER = "[ERROR] 숫자만 입력할 수 있습니다.";
     public static final String ONLY_CORRECT_MONEY = "[ERROR] 1원 이상 100,000,000원 이하만 입력할 수 있습니다.";
+    public static final String ONLY_YES_OR_NO = "[ERROR] y혹은 n을 입력하세요.";
 }
