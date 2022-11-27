84af4488f75fe8f6b1120d1f3e9b50b61c018a48
Isu Shin <isu010614@naver.com>
2022-11-28 01:45:06 +0900

    feat: getter 메서드 추가

--- src/main/java/domain/user/Player.java
+++ src/main/java/domain/user/Player.java
@@ -4,6 +4,7 @@ import domain.card.Card;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * 게임 참여자를 의미하는 객체
@@ -20,6 +21,20 @@ public class Player {
 
     public void addCard(Card card) {
         cards.add(card);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getPlayerCards() {
+        return cards.stream()
+                .map(Card::toString)
+                .collect(Collectors.joining(", "));
+    }
+
+    public List<Card> getCardsInList(){
+        return cards;
     }
 
     // TODO 추가 기능 구현
