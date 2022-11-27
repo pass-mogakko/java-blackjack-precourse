0256c905c1c0bef454f7873b429d60f223e458a4
yoondgu <doyou2ing@naver.com>
2022-11-28 02:25:42 +0900

    :sparkles:feat(Domain-9): 딜러, 플레이어의 수익을 저장한 뒤 더하거나 빼는 기능 구현

--- src/main/java/model/Earnings.java
+++ src/main/java/model/Earnings.java
@@ -12,8 +12,18 @@ public class Earnings {
 
     public Earnings(List<String> playerNames) {
         this.dealerEarning = 0;
+        validatePlayerNames(playerNames);
         this.playerEarningsByName = playerNames.stream()
                 .collect(Collectors.toMap(Function.identity(), name -> 0.0));
+    }
+
+    private void validatePlayerNames(List<String> playerNames) {
+        if (playerNames == null) {
+            throw new NullPointerException("게임 참가자 이름 정보가 null입니다.");
+        }
+        if (playerNames.isEmpty()) {
+            throw new IllegalArgumentException("게임 참가자 이름 정보가 존재하지 않습니다.");
+        }
     }
 
     public double getDealerEarning() {
@@ -21,7 +31,20 @@ public class Earnings {
     }
 
     public double findPlayerEarningByName(String playerName) {
-        return playerEarningsByName.get(playerName);
+        Double earning = playerEarningsByName.get(playerName);
+        if (earning == null) {
+            throw new IllegalArgumentException("해당 이름을 가진 사용자가 존재하지 않습니다.");
+        }
+        return earning;
+    }
+
+    public void moveEarningFromDealerToPlayer(String playerName, double amount) {
+        dealerEarning -= amount;
+        updatePlayerEarningsByName(playerName, amount);
+    }
+
+    public void updatePlayerEarningsByName(String playerName, double amount) {
+        playerEarningsByName.put(playerName, findPlayerEarningByName(playerName) + amount);
     }
 
     @Override
---
+++ src/test/java/model/EarningsTest.java
@@ -0,0 +1,44 @@
+package model;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+class EarningsTest {
+
+    @DisplayName("수익 정보 클래스 생성: 전달받은 사용자 이름 리스트가 null 또는 비어있을 경우 예외 발생")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void createEarnings(List<String> playerNames) {
+        assertThatThrownBy(() -> new Earnings(playerNames))
+                .isInstanceOfAny(NullPointerException.class, IllegalArgumentException.class);
+    }
+
+    @DisplayName("수익 업데이트: 존재하지 않는 이름의 사용자 수익 조회 시 예외 발생")
+    @Test
+    void findPlayerEarningByName() {
+        Earnings earnings = new Earnings(List.of("pobi", "jason", "joon"));
+
+        assertThatThrownBy(() -> earnings.findPlayerEarningByName("wrong"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThat(earnings.findPlayerEarningByName("pobi")).isEqualTo(0);
+    }
+
+    @DisplayName("수익 업데이트: 특정 금액을 사용자 수익에는 더하고, 딜러 수익에는 빼도록 계산")
+    @ParameterizedTest
+    @ValueSource(doubles = {10_000, -5_000, -1_000_000})
+    void updateEarning(double amount) {
+        Earnings earnings = new Earnings(List.of("pobi", "jason", "joon"));
+        earnings.moveEarningFromDealerToPlayer("pobi", amount);
+
+        assertThat(earnings.getDealerEarning()).isEqualTo((-1)*amount);
+        assertThat(earnings.findPlayerEarningByName("pobi")).isEqualTo(amount);
+    }
+}
