5529ab6d1a03149ab51526e7b56fe4554e673ff9
Isu Shin <isu010614@naver.com>
2022-11-27 17:57:47 +0900

    feat: 플레이어 이름 입력 예외처리 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,7 @@
 package view;
 
+import domain.exception.ExceptionMessage;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,6 +11,7 @@ import java.util.StringTokenizer;
 
 public class InputView {
     private static final String INPUT_PLAYER_NAME = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String NAME_REGEX = "[a-z]";
 
     private static final BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 
@@ -17,9 +20,17 @@ public class InputView {
         List<String> playerName = new ArrayList<>();
         StringTokenizer tokenizer = new StringTokenizer(console.readLine(), ",");
         while (tokenizer.hasMoreTokens()) {
-            playerName.add(tokenizer.nextToken());
+            String name = tokenizer.nextToken();
+            validateName(name);
+            playerName.add(name);
         }
         return playerName;
+    }
+
+    private static void validateName(String name) {
+        if (!name.matches(NAME_REGEX)) {
+            throw new IllegalArgumentException(ExceptionMessage.ONLY_ALPHABET);
+        }
     }
 
 }
