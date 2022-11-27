66b290f23f0aa572bfa69764238e7456a0fa00bc
Isu Shin <isu010614@naver.com>
2022-11-28 02:30:40 +0900

    test: 카드 덱 생성 테스트 코드 수정

--- src/test/java/domain/card/CardFactoryTest.java
+++ src/test/java/domain/card/CardFactoryTest.java
@@ -2,15 +2,16 @@ package domain.card;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CardFactoryTest {
     @Test
-    void create() {
-        List<Card> cards = CardFactory.create();
-        assertThat(cards).hasSize(52);
-        System.out.println(cards);
+    void validateCreateDeck() {
+        CardFactory cardFactory = new CardFactory();
+        HashMap<Integer, Card> deck = cardFactory.createDeck();
+        assertThat(deck).hasSize(52);
     }
 }
