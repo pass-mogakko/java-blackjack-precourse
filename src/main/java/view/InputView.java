e79605dd5476f8a54e2f96c391e6995cf4916318
Isu Shin <isu010614@naver.com>
2022-11-28 02:14:54 +0900

    feat: 명령 예외처리 기능 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -12,10 +12,13 @@ import java.util.StringTokenizer;
 public class InputView {
     private static final String INPUT_PLAYER_NAME = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private static final String INPUT_BETTING_MONEY = "의 베팅금액은?";
+    private static final String INPUT_COMMAND = "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
     private static final String NAME_REGEX = "^[a-z]+$";
     private static final String MONEY_REGEX = "^[0-9]+$";
     private static final int START_MONEY_RANGE = 0;
     private static final int END_MONEY_RANGE = 100_000_000;
+    private static final String YES_COMMAND = "y";
+    private static final String NO_COMMAND = "n";
 
     private static final BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
 
@@ -55,6 +58,21 @@ public class InputView {
     private static void validateMoneyRange(int money) {
         if (money <= START_MONEY_RANGE || money > END_MONEY_RANGE) {
             throw new IllegalArgumentException(ExceptionMessage.ONLY_CORRECT_MONEY);
+        }
+    }
+
+    public static String readCommand(String name) throws IOException {
+        System.out.print(System.lineSeparator());
+        System.out.println(name + INPUT_COMMAND);
+        String command = console.readLine();
+        validateCommand(command);
+        return command;
+    }
+
+    private static void validateCommand(String command) {
+        if(!command.equals(YES_COMMAND)
+                && !command.equals(NO_COMMAND)){
+            throw new IllegalArgumentException(ExceptionMessage.ONLY_YES_OR_NO);
         }
     }
 }
