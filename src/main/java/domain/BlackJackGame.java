9726b7b8bccb234a8d63b99111b6437b67dc84c4
Isu Shin <isu010614@naver.com>
2022-11-27 19:03:40 +0900

    feat: 카드 덱을 생성하는 기능

---
+++ src/main/java/domain/BlackJackGame.java
@@ -0,0 +1,14 @@
+package domain;
+
+import domain.card.Card;
+import domain.card.CardFactory;
+
+import java.util.HashMap;
+
+public class BlackJackGame {
+    private static final HashMap<Integer, Card> deck = CardFactory.createDeck();
+
+    public static void run(){
+
+    }
+}
