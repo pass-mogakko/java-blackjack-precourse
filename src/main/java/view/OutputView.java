675539e806f86a6d4278fb09f27fd7decaf3d0b1
Isu Shin <isu010614@naver.com>
2022-11-28 01:40:36 +0900

    refactor: 카드 출력 메서드 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,5 +1,6 @@
 package view;
 
+import domain.BlackJackGame;
 import domain.user.Dealer;
 import domain.user.Player;
 
@@ -7,24 +8,43 @@ import java.util.List;
 
 public class OutputView {
     private static final String DEALER = "딜러";
-    private static final String COLON = ":";
+    private static final String COLON = ": ";
     private static final String CARD = "카드";
     private static final String AND = "와 ";
     private static final String HANDOUT_CARDS_MESSAGE = "에게 2장의 카드를 나누었습니다.";
 
-    public void printHandOutMessage(List<String> playerName) {
+    public static void printHandOutCards(BlackJackGame game) {
+        System.out.print(System.lineSeparator());
+        printHandOutMessage(game.getPlayers());
+        printDealerCard(game.getDealer());
+        printPlayerCard(game.getPlayers());
+    }
+
+    private static void printHandOutMessage(List<Player> players) {
         System.out.print(DEALER + AND);
-        System.out.print(String.join(", ", playerName));
+        System.out.print(printPlayerNames(players));
         System.out.println(HANDOUT_CARDS_MESSAGE);
     }
 
-    public void printDealerCard(Dealer dealer) {
+    private static StringBuilder printPlayerNames(List<Player> players) {
+        StringBuilder playerNames = new StringBuilder();
+        for (Player player : players) {
+            playerNames.append(player.getName()).append(", ");
+        }
+        playerNames.deleteCharAt(playerNames.lastIndexOf(" "));
+        playerNames.deleteCharAt(playerNames.lastIndexOf(","));
+        return playerNames;
+    }
+
+    private static void printDealerCard(Dealer dealer) {
         System.out.print(DEALER + COLON);
         System.out.println(dealer.getInitialDealerCard());
     }
 
-    public void printPlayerCard(Player player) {
-        System.out.print(player.getName() + CARD + COLON);
-        System.out.println(player.getPlayerCards());
+    private static void printPlayerCard(List<Player> players) {
+        for (Player player : players) {
+            System.out.print(player.getName() + CARD + COLON);
+            System.out.println(player.getPlayerCards());
+        }
     }
 }
