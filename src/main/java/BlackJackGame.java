008af720cfcfef7ab83f898bc1872683ad332cd4
Isu Shin <isu010614@naver.com>
2022-11-27 18:45:14 +0900

    feat: 플레이어 각각의 베팅 금액을 리스트에 저장하는 기능 구현

--- src/main/java/BlackJackGame.java
+++ src/main/java/BlackJackGame.java
@@ -1,11 +1,16 @@
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
+import static view.InputView.readBettingMoney;
 import static view.InputView.readPlayerName;
 
 public class BlackJackGame {
     public static void main(String[] args) {
-        System.out.println(getPlayerName());
+        List<String> playerName = getPlayerName();
+        List<Integer> bettingMoney = getBettingMoneyInList(playerName);
+        System.out.println(bettingMoney);
+
     }
 
     private static List<String> getPlayerName(){
@@ -14,6 +19,23 @@ public class BlackJackGame {
         }catch(IllegalArgumentException | IOException e){
             System.out.println(e.getMessage());
             return getPlayerName();
+        }
+    }
+
+    private static List<Integer> getBettingMoneyInList(List<String> playerName){
+        List<Integer> bettingMoney = new ArrayList<>();
+        for(String name: playerName){
+            bettingMoney.add(getBettingMoney(name));
+        }
+        return bettingMoney;
+    }
+
+    private static int getBettingMoney(String name){
+        try{
+            return readBettingMoney(name);
+        }catch(IllegalArgumentException | IOException e){
+            System.out.println(e.getMessage());
+            return getBettingMoney(name);
         }
     }
 }
