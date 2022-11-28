994bcfe6954b5c1cb1855809f2927e6afb634a48
Isu Shin <isu010614@naver.com>
2022-11-29 02:41:34 +0900

    feat: 베팅 금액 계산 기능

--- src/main/java/domain/user/Player.java
+++ src/main/java/domain/user/Player.java
@@ -13,6 +13,10 @@ public class Player {
     private final String name;
     private final double bettingMoney;
     private final List<Card> cards = new ArrayList<>();
+    private double money = 0;
+    final int INIT_CARDS_COUNT = 2;
+    final double INIT_BLACKJACK_MULTIPLE = 2;
+    final double BLACKJACK_MULTIPLE = 1.5;
 
     public Player(String name, double bettingMoney) {
         this.name = name;
@@ -47,6 +51,30 @@ public class Player {
             count += card.getSymbolScore();
         }
         return count;
+    }
+
+    public boolean isTwoCards() {
+        return cards.size() == INIT_CARDS_COUNT;
+    }
+
+    public void loseAll() {
+        money -= bettingMoney;
+    }
+
+    public void initBlackJack() {
+        money += bettingMoney * BLACKJACK_MULTIPLE;
+    }
+
+    public void blackJack() {
+        money += bettingMoney * INIT_BLACKJACK_MULTIPLE;
+    }
+
+    public void earnBettingMoney() {
+        money += bettingMoney;
+    }
+
+    public double getPlayerMoney() {
+        return money;
     }
 
     // TODO 추가 기능 구현
