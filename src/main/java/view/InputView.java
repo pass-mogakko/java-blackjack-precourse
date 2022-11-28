2d3bda23f22fd01323c6fcfaaf105ecfe07bac06
jindaram-stu <jinsb1999@naver.com>
2022-11-28 22:39:59 +0900

    feat: 카드 추가 배분 여부 입력 기능 추가
    y 또는 n의 명령어를 입력하는 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,15 +1,19 @@
 package view;
 
+import constants.View;
+import domain.MoreCard;
+import domain.user.Player;
 import dto.NewPlayerDTO;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
-import util.Converter;
+import util.converter.NewPlayerConverter;
 
 public class InputView {
 
     private final static String INPUT_PLAYERS = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
     private final static String INPUT_BETTING_AMOUNT = "%s의 배팅 금액은?";
+    private final static String INPUT_MORE_CARDS = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
 
     private final static String ARRAY_DIVIDER = ",";
 
@@ -17,18 +21,21 @@ public class InputView {
         System.out.println(INPUT_PLAYERS);
         List<String> players = Arrays.asList(readLine().split(ARRAY_DIVIDER));
         // validate
-        return Converter.convertNewPlayersDTO(players);
+        return NewPlayerConverter.convertNewPlayersDTO(players);
     }
 
     public int inputBettingAmount(NewPlayerDTO newPlayerDTO) {
         System.out.printf(INPUT_BETTING_AMOUNT,newPlayerDTO.getName());
         String bettingMoney = readLine();
         // validate
-        return Converter.convertBettingMoney(bettingMoney);
+        return NewPlayerConverter.convertBettingMoney(bettingMoney);
     }
 
-    public void inputMoreMoreDivide() {
-
+    public MoreCard inputMoreMoreDivide(Player player) {
+        System.out.printf(INPUT_MORE_CARDS + View.LINE_BREAK,player.getName());
+        String command = readLine();
+        // validate
+        return MoreCard.getMoreCardByCommand(command);
     }
 
     private String readLine() {
