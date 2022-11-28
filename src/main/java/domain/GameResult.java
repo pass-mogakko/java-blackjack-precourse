8dc4ee773dcb49970d05211febb8bcfafe58c12f
Isu Shin <isu010614@naver.com>
2022-11-29 02:37:52 +0900

    feat: 플레이어와 딜러의 점수를 계산하는 기능

--- src/main/java/domain/GameResult.java
+++ src/main/java/domain/GameResult.java
@@ -3,22 +3,36 @@ package domain;
 import domain.user.Dealer;
 import domain.user.Player;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameResult {
     private final List<Player> players;
     private final Dealer dealer;
+    private final List<Integer> score = new ArrayList<>();
 
     public GameResult(List<Player> players, Dealer dealer) {
         this.players = players;
         this.dealer = dealer;
     }
 
-    public int getPlayerTotalScore(Player player){
-        return player.calculatePlayerScore();
+    public List<Integer> getPlayersScore() {
+        return score;
     }
 
-    public int getDealerTotalScore(){
+    private void setPlayerScore() {
+        for (Player player : players) {
+            score.add(getPlayerTotalScore(player));
+        }
+    }
+
+    private int getDealerTotalScore() {
+        assert dealer != null;
         return dealer.calculateDealerScore();
+    }
+
+    private int getPlayerTotalScore(Player player) {
+        assert player != null;
+        return player.calculatePlayerScore();
     }
 }
